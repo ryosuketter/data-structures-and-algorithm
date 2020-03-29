234805ad283dc609361bad55fb5ad6cd0d37a14e
ryosuketter <r.shimizu.yale@gmail.com>
2020-03-30 01:15:42 +0900

    add insertion sort code

---
+++ Chapter3/ALDS1_1_A_Insertion_Sort.js
@@ -0,0 +1,25 @@
+const insertionSort = input => {
+  let list = input
+    .split("\n")[1]
+    .split(" ")
+    .map(x => parseInt(x));
+  console.log(list.join(" "));
+
+  for (let i = 1; i < list.length; i++) {
+    let el = list[i];
+    let j;
+
+    for (j = i - 1; j >= 0 && list[j] > el; j--) {
+      list[j + 1] = list[j];
+    }
+    list[j + 1] = el;
+    console.log(list.join(" "));
+  }
+};
+
+insertionSort(
+  require("fs").readFileSync(
+    "/Users/ryosuke/Project/data-structures-and-algorithm/Chapter3/sample.txt",
+    "UTF-8"
+  )
+);
